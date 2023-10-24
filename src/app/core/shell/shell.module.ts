3ce42104ad1c832ebc26319b7af150e4b649b271
Felipe Maines <felipemaines123@gmail.com>
2023-10-24 20:23:20 -0300

    add: listagem, edicao, adicao, ex de categoria

---
+++ src/app/core/shell/shell.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShellComponent } from './shell.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    ShellComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  exports: [ShellComponent]
+})
+export class ShellModule { }
